887f5fcf2fb951e1277870ad8d81baf288f1eb4f
UMI ARINA <umiarina1101@gmail.com>
2026-06-11 20:06:53 +0700

    Update database.php

--- aic_fashion_metro/config/database.php
+++ aic_fashion_metro/config/database.php
@@ -1,9 +1,26 @@
 <?php
 
-$conn = mysqli_connect("localhost","root","","aic_fashion_metro");
+$mysql_url = $_ENV['MYSQL_URL'] ?? getenv('MYSQL_URL');
 
-if(!$conn){
-    die("Koneksi gagal : " . mysqli_connect_error());
+if ($mysql_url) {
+    $parts = parse_url($mysql_url);
+    $host  = $parts['host'];
+    $user  = $parts['user'];
+    $pass  = $parts['pass'];
+    $db    = ltrim($parts['path'], '/');
+    $port  = $parts['port'] ?? 3306;
+} else {
+    $host = 'localhost';
+    $user = 'root';
+    $pass = '';
+    $db   = 'aic_fashion';
+    $port = 3306;
 }
 
-?>
+$conn = new mysqli($host, $user, $pass, $db, (int)$port);
+
+if ($conn->connect_error) {
+    die('Koneksi gagal: ' . $conn->connect_error);
+}
+
+$conn->set_charset('utf8mb4');
